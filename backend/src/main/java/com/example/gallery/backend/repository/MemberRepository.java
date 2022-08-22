48eeb4b206ed937a464dc1be57900c6095625f6c
freewisl <freewisl@nate.com>
2022-08-22 22:56:19 +0900

    Initial commit

---
+++ backend/src/main/java/com/example/gallery/backend/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.gallery.backend.repository;
+
+import com.example.gallery.backend.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Member findByEmailAndPassword(String email, String password);
+}
---
+++ backend/src/main/java/com/example/gallery/backend/service/impl/JwtServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.gallery.backend.service.impl;
+
+import com.example.gallery.backend.service.JwtService;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service("jwtService")
+public class JwtServiceImpl implements JwtService {
+
+    private String secretKey = "asdfdsf3r543#jgk^^%jnkjnhkals303T@#s#$@$34sdf4fjkjuy64.,dfasdfjkhsdkfajhasldkfjafl!@#&&$$#$DFASF";
+
+    @Override
+    public String getToken(String key, Object value) {
+
+        Date expTime = new Date();
+        expTime.setTime(expTime.getTime() + 1000 * 60 * 5);
+        byte[] secretByteKey = DatatypeConverter.parseBase64Binary(secretKey);
+        Key signKey = new SecretKeySpec(secretByteKey, SignatureAlgorithm.HS256.getJcaName());
+
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("typ", "JWT");
+        headerMap.put("alg", "HS256");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put(key, value);
+
+        JwtBuilder builder = Jwts.builder().setHeader(headerMap).setClaims(map).setExpiration(expTime).signWith(signKey, SignatureAlgorithm.HS256);
+
+        return builder.compact();
+    }
+
+    @Override
+    public Claims getClaims(String token) {
+        if(token != null && !"".equals(token)){
+            try {
+                byte[] secretByteKey = DatatypeConverter.parseBase64Binary(secretKey);
+                Key signKey = new SecretKeySpec(secretByteKey, SignatureAlgorithm.HS256.getJcaName());
+                return Jwts.parserBuilder().setSigningKey(signKey).build().parseClaimsJws(token).getBody();
+            } catch(ExpiredJwtException e) {
+                //만료됨
+            } catch (JwtException e){
+                // 유효하지 않음
+            }
+        }
+        return null;
+    }
+}
